cf25cc2a03410e0cd552920b98395049b977b46b
dnfive <dnfive17@yandex.ru>
2023-11-18 13:01:22 +0300

    Сделал главное меню в графическом интерфейсе, вывод карт, выпуск новой карты, выход из приложения

--- gui.py
+++ gui.py
@@ -37,15 +37,11 @@ class LoginApp:
 		if result != "Successfull":
 			messagebox.showerror("Ошибка входа", result)
 		else:
-			account = Account(username, password)
+			self.account = Account(username, password)
 			messagebox.showinfo("Успешный вход", "Добро пожаловать, {}".format(username))
+			root.destroy
+			self.open_main_window()
 
-		
-		# Пример простой проверки
-		# if username == "user" and password == "password":
-		#     messagebox.showinfo("Успешный вход", "Добро пожаловать, {}".format(username))
-		# else:
-		#     messagebox.showerror("Ошибка входа", "Неверный логин или пароль")
 
 	def registration(self):
 		reg_login = self.reg_login_var.get()
@@ -96,6 +92,67 @@ class LoginApp:
 
 		# Кнопка для завершения регистрации
 		tk.Button(registration_window, text="Зарегистрироваться", command=self.registration).pack(pady=10)
+
+	def open_main_window(self):
+		self.main_window = tk.Toplevel(self.root)
+		self.main_window.title("LegionBank - главная")
+		self.main_window.geometry("500x500")
+
+		# Создание метки с заголовком
+		label = tk.Label(self.main_window, text="Личный кабинет - dnfive", font=("Arial", 20))
+		label.pack(pady=10)
+
+		# Создание кнопок
+		button1 = tk.Button(self.main_window, text="Мои карты", command=self.open_my_cards)
+		button1.pack(pady=5)
+
+		button2 = tk.Button(self.main_window, text="Выпуск новой карты", command=self.issue_new_card)
+		button2.pack(pady=5)
+
+		button3 = tk.Button(self.main_window, text="Переводы", command=self.open_transfers)
+		button3.pack(pady=5)
+
+		button4 = tk.Button(self.main_window, text="Выход", command=self.exit_app)
+		button4.pack(pady=5)
+
+	def open_my_cards(self):
+		my_cards = tk.Toplevel(self.root)
+		my_cards.title("LegionBank - Мои карты")
+		my_cards.geometry("600x400")
+
+		self.cursor.execute("""
+			SELECT * FROM cards WHERE owner = ?
+		""", [self.account.login])
+		userсards = self.cursor.fetchall()
+		for item in userсards:
+			label = tk.Label(my_cards, text="ID карты: " + str(item[0]) + " Номер карты: " + str(item[1]) + "Остаток: " + str(item[3]), font=("Arial", 8))
+			label.pack(pady=10)
+
+
+	def issue_new_card(self):
+		owner = self.account.login
+		date = datetime.datetime.now().strftime("%d-%m-%Y %H:%M") # Получение текущей даты по шаблону
+		numcard = hashlib.md5(date.encode('utf-8')).hexdigest() # Хеширование даты
+		try:
+			self.cursor.execute("""
+				INSERT INTO cards (numcard, owner, balance, date)
+				VALUES (?, ?, ?, ?)
+			""", (numcard, owner, 0.0, date))
+		except sqlite3.DatabaseError as err:
+				messagebox.showerror("Ошибка базы данных", err)
+		else:
+			messagebox.showinfo("Выпуск карты", "Карта {} успешно выпущена".format(numcard))
+			self.conn.commit()
+
+	def open_transfers(self):
+		print("Открытие страницы 'Переводы'")
+		# Добавьте свой код открытия страницы "Переводы" здесь
+
+	def exit_app(self):
+		print("Выход из приложения")
+		messagebox.showinfo("Выход", "До свидания, {}".format(self.account.login))
+		self.main_window.destroy()
+
 
 if __name__ == "__main__":
 	root = tk.Tk()
